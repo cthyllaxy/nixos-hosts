7d170c215ee41725dc30a7d297b7cf05bcb4f670
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-02-12 20:47:14 -0500

    feat: update user config on zoth-ommog

--- hosts/zoth-ommog/configuration.nix
+++ hosts/zoth-ommog/configuration.nix
@@ -52,7 +52,7 @@
 
   # nixos modules
   nixosModules = {
-    users.natalie.enable = true;
+    users.usernames = ["thamenato" "nmeusling"];
     steam = {
       enable = true;
       gamescope = true;
