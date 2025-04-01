860c119778521445d73506269c8f3708f5212c89
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-04-01 18:35:07 -0400

    feaet: update to kernel 6.14

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -4,6 +4,6 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_6_13;
+    kernelPackages = pkgs.linuxPackages_6_14;
   };
 }
