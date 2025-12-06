d734498a87d1424618f24227c916250313300783
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-12-06 13:40:13 -0500

    fix: add niri cache

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -18,12 +18,12 @@
       substituters = [
         "https://nix-cache.cthyllaxy.xyz"
         "https://nix-community.cachix.org"
-        "https://ghostty.cachix.org"
+        "https://niri.cachix.org"
       ];
       trusted-public-keys = [
         "nix-cache.cthyllaxy.xyz:CEJYeiGUveq4GMALY2GHhcIwrr5PwYwdUj6skoHmBH8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       ];
     };
     # optimise.automatic = true;
