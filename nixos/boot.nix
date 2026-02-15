f086d1a27b86edf8d5b3de663804e32af2cd2929
Thales Menato <8753631+thamenato@users.noreply.github.com>
2026-02-15 11:28:26 -0500

    feat: update to kernel 6.19

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -4,6 +4,6 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_6_18;
+    kernelPackages = pkgs.linuxPackages_6_19;
   };
 }
